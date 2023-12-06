b2b8861d96f29cf1355a5fc94a644e1956d04887
Simon Brage <simonbrage@live.dk>
2023-12-07 00:05:01 +0100

    Imported more evaluation metrics

--- src/utils/imports.py
+++ src/utils/imports.py
@@ -20,7 +20,8 @@ from torchvision import models, transforms  # Import torchvision and transforms
 from torchvision.models import ResNet18_Weights, ResNet34_Weights, ResNet50_Weights, ResNet101_Weights, ResNet152_Weights, AlexNet_Weights
 from mpl_toolkits.mplot3d import Axes3D
 from PIL import Image
-from sklearn.metrics import confusion_matrix, mean_squared_error
+from scipy.stats import pearsonr
+from sklearn.metrics import confusion_matrix, mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
 from torchvision.utils import make_grid
 from torch.utils.data import DataLoader, Dataset

b1071601f0b8374d493b0ef384b92ceeff0a33f0
Simon Brage <simonbrage@live.dk>
2023-12-07 15:38:23 +0100

    Ran all models

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,4 +1,4 @@
 # Variables
 batch_size = 128
-num_epochs = 300
-model_str = "AlexNet" # Can be "AlexNet", "ResNet18" or "ResNet50"
+num_epochs = 50
+model_str = "ResNet50" # Can be "AlexNet", "ResNet18" or "ResNet50"

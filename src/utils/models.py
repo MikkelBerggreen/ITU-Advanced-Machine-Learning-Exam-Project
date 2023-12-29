039311d73055f8aaef1ff751f9309adc23cdb90b
kasper henningsen <sejekasper@gmail.com>
2023-12-29 19:36:10 +0100

    Implemented Gabobor Wavelets

--- src/utils/models.py
+++ src/utils/models.py
@@ -4,24 +4,39 @@ import hashlib
 class PretrainedModel(nn.Module):
            
 
-    def __init__(self, model_str):
+    def __init__(self, model_str, output_size, dropout):
         super(PretrainedModel, self).__init__()
         self.features_cache = {}
 
         if model_str == "AlexNet":
             base_model = models.alexnet(weights=AlexNet_Weights.DEFAULT)
-            self.features = base_model.features
+            #self.features = base_model.features
+            self.features = nn.Sequential(
+                nn.Conv2d(18, 64, kernel_size=11, stride=4, padding=2),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2),
+                nn.Conv2d(64, 192, kernel_size=5, padding=2),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2),
+                nn.Conv2d(192, 384, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(384, 256, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(256, 256, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.MaxPool2d(kernel_size=3, stride=2),
+            )
 
             self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
             self.classifier = nn.Sequential(
-                nn.Dropout(0.82),
+                nn.Dropout(dropout),
                 nn.Linear(256 * 6 * 6, 4096),
-                nn.Dropout(0.82),
+                nn.Dropout(dropout),
                 nn.ReLU(inplace=True),
-                nn.Linear(4096, 4096),
-                nn.Dropout(0.82),
+                nn.Linear(4096,4096),
+                nn.Dropout(dropout),
                 nn.ReLU(inplace=True),
-                nn.Linear(4096, 70),
+                nn.Linear(4096, output_size),
             )
 
 
